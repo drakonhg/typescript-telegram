8efa8bb0fa2a9aa90dcc3aefe8a99445203760fb
drakonhg <drakonhg@gmail.com>
2021-09-03 16:37:26 +1000

    Testing TS action

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import * as core from '@actions/core'
 
 async function run(): Promise<void> {
   try {
+    core.debug('It is working')
     const name: string = core.getInput('name')
     core.debug(name.big())
   } catch (error) {
